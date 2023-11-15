07a04ac77f8f5d87a6c9269c2ee9d11927816ac6
shunriokuhira <shunriaoping99@gmail.com>
2023-11-15 17:15:14 +0900

    commit CONTROL 2

--- CONTROL/CONTROL.v
+++ CONTROL/CONTROL.v
@@ -1,13 +1,13 @@
 module CONTROL(
     input [2:0] SEL_IN,
-    input CLK, RST, DEC_IN, CLR_IN, QUE_IN,
+    input CLK, RST, DEC_IN, CLR_IN, QUE_IN, READY_IN,
     input OK_IN, //二人ともスタート押して、READYモジュールからその信号を受け取る。
     input [1:0] HP_IN,
     input QUE,//入力側に問題が入ってれば値は1、入ってなければ0
     input JUDG_IN,  //勝利判定結果の信号受取
     input WRONG_IN, //回答を間違えたときの信号受取
-    output READY,
-    output [3:0] STATE,
+    output reg READY_OUT,
+    output reg [3:0] STATE, //
     output reg [2:0] SEL_OUT,
     output reg DEC_OUT, CLR_OUT 
 );
@@ -17,14 +17,16 @@ localparam  READY = 4'b0010, QUESTION = 4'b0011, INPUT = 4'b0100, DRAW = 4'b0110
             , WRONG = 4'b0111, GOOD = 4'b1000, OUCH = 4'b1001, WIN = 4'b1010, LOSE = 4'b1011;
 
 reg R1 , R2, R3;
-	initial begin 
+initial begin 
 	 R1 = 0; //SEL_IN[0]
 	 R2 = 0; //SEL_IN[1]
 	 R3 = 0; //SEL_IN[2]
      R4 = 0; //DEC_IN
      R5 = 0; //CLR_IN
      R6 = 0; //QUE_IN
-	end
+
+
+end
 	
 always @(posedge CLK) begin   //Generate Toggle Wave → 信号を出しぱなしにする
     if(R1 == 0 && SEL_IN[0]) //最初は上記initial文より、R1の値は0です。そこで、SWをオンにして、SEL_IN[0]が1になったら、R1は1を放出し続けます。
@@ -68,6 +70,15 @@ always @(posedge CLK) begin   //Generate Toggle Wave
 		R6 <= 0;
 end
 
+
+always @(posedge CLK)begin //output
+    READY_OUT <= READY_IN;
+    SEL_OUT <= SEL_IN;
+    DEC_OUT <= DEC_IN;
+    CLR_OUT <= CLR_IN;
+end
+
+
 always @( posedge CLK) begin //
 	  if(RST)
 		 cur <= READY;
@@ -75,19 +86,26 @@ always @( posedge CLK) begin //
 		 cur <= nxt;
 end
 
+
+reg NEED_1SEC;//タイマーを動かすためのフラグ
+
 always @(posedge CLK)begin
-    case(cur)
-        READY: if(OK_IN && QUE) //二人ともスタート押した、かつ入力モジュールに問題が格納されている
+    NEED_1SEC <= 0;
+    case(cur)   
+        READY:  STATE <= 4'b0010;//入力モジュールに現在の状態をわたす。
+                if(OK_IN && QUE) //二人ともスタート押した、かつ入力モジュールに問題が格納されている
                     nxt <= QUESTION; //問題表示
                 else
                     nxt <= READY;
 
-        QUESTION: if(R6 == 1 && QUE) //入力画面に遷移するボタン押す、かつ入力モジュールに問題が格納されている
+        QUESTION: STATE <= 4'b0011;
+                   if(R6 == 1 && QUE) //入力画面に遷移するボタン押す、かつ入力モジュールに問題が格納されている
                     nxt <= INPUT;
                    else 
                     nxt <= QUESTION;
         
-        INPUT: if(R6 == 0 && QUE)
+        INPUT:  STATE <= 4'b0100;
+                if(R6 == 0 && QUE)
                     nxt <= QUESTION;
                 else if(WRONG_IN)
                     nxt <= WRONG;
@@ -100,36 +118,48 @@ always @(posedge CLK)begin
                 else
                     nxt <= INPUT;
 
-        WRONG: if(1秒後)
+        WRONG: STATE <= 4'b0111;
+               NEED_1SEC <= 1;
+               if(EN1HZ)
                 nxt <= INPUT;
                else 
                 nxt <= WRONG;
         
-        GOOD: if(1秒後)
+        GOOD: STATE <= 4'b1000;
+              NEED_1SEC <= 1;
+              if(EN1HZ)
                 nxt <= READY;
               else if(HP_IN == 01) 
                 nxt <= WIN;
               else
                 nxt <= GOOD;
 
-        OUCH: if(1秒後)
+        OUCH: STATE <= 4'b1001;
+              NEED_1SEC <= 1;
+              if(EN1HZ)
                 nxt <= READY;
               else if(HP_IN == 10)
                 nxt <= LOSE;
               else 
                 nxt <= OUCH;
 
-        DRAW: if(1秒後)
+        DRAW: STATE <= 4'0110;
+              NEED_1SEC <= 1;
+              if(EN1HZ)
                 nxt <= READY;
               else
                 nxt <= DRAW;
 
-        WIN: if(1秒後)
+        WIN: STATE <= 4'b1010;
+             NEED_1SEC <= 1;
+             if(EN1HZ)
                nxt <= READY;
              else 
                nxt <= WIN;
 
-        LOSE: if(1秒後)
+        LOSE:STATE <= 4'b1011; 
+             NEED_1SEC <= 1; 
+             if(EN1HZ)
                nxt <= READY;
              else 
                nxt <= LOSE;
@@ -138,8 +168,22 @@ always @(posedge CLK)begin
 end
 
 
+reg [25:0] cnt;
+initial begin
+    cnt <= 0;
+end
 
+always @(posedge CLK)begin
+    if(RST)
+      cnt <= 26'0;
+    else if(EN1HZ || NEED_1SEC == 0)
+      cnt <= 26'0;
+    else if(NEED_1SEC == 1)
+      cnt <= cnt + 26'b1; 
+    else 
+      cnt <= cnt;   
+end
 
-
+assign EN1HZ = (cnt==26'd49_999_999);
 
 endmodule
