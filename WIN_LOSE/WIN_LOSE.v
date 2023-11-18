11cfde0ca970548e17204e217260c6813cc78532
KadoyaHarutake <250302g@ugs.kochi-tech.ac.jp>
2023-11-18 16:21:35 +0900

    modified bit weight

--- WIN_LOSE/WIN_LOSE.v
+++ WIN_LOSE/WIN_LOSE.v
@@ -1,23 +1,23 @@
 module WIN_LOSE ( //どっちの正解が早かったかというモジュール
     input   CLK,RST,
-    input   [1:0] MINE ,//正誤判定のoutput
-    input   [1:0] ENEMY, //相手の正解を受け取る   1が正解
-    output  reg [2:0] WL_OUT //HP管理に送られる 00 無入力　01 自分勝ち 10　相手勝ち　11　ドロー
+    input   MINE ,//正誤判定のoutput
+    input   ENEMY, //相手の正解を受け取る   1が正解 [1:0]が不要かも
+    output  reg [1:0] WL_OUT //HP管理に送られる 00 無入力　01 自分勝ち 10　相手勝ち　11　ドロー
 );
 
 initial begin
- WL_OUT <= 3'b00;
+ WL_OUT <= 2'b00;
  end   
 
 always @(posedge CLK) begin
     if(ENEMY && MINE)
-        WL_OUT <= 3'b11; //ドロー
+        WL_OUT <= 2'b11; //ドロー
     else if(ENEMY == 1 && MINE == 0)
-        WL_OUT <= 3'b10; //相手勝ち
+        WL_OUT <= 2'b10; //相手勝ち
     else if(MINE  == 1 && ENEMY == 0)
-        WL_OUT <= 3'b01; //自分勝ち
+        WL_OUT <= 2'b01; //自分勝ち
     else
-        WL_OUT <= 3'b00; //無入力、その他
+        WL_OUT <= 2'b00; //無入力、その他
           
 end
 
