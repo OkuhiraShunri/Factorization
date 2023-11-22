93dac56dcb7ebc0469186f4905017b0a7127d620
KadoyaHarutake <250302g@ugs.kochi-tech.ac.jp>
2023-11-22 16:53:38 +0900

    1PTEST commit

--- 1PTEST/INPUT.v
+++ 1PTEST/INPUT.v
@@ -1,14 +1,15 @@
 module INPUT(
-    input [2:0] SEL_IN,//SW[9] = SEL_IN[2]:HEX[4], SW[8] = SEL_IN[1]:HEX[2], SW[7] = SEL_IN[0]:HEX[0]
-    input INC_IN, DEC_IN, CLK, RST,
-	 input [1:0] RESULT_IN, 
+    input [2:0] SEL,//SW[9] = SEL[2]:HEX[4], SW[8] = SEL[1]:HEX[2], SW[7] = SEL[0]:HEX[0]
+    input [3:0] STATE,
+    input  DEC, CLK, RST,
+	 //input [1:0] RESULT, 
     output reg [3:0] SEG1, SEG2, SEG3, SEG4, SEG5, SEG6,
 	 output reg [3:0] COUNT1_out, COUNT2_out, COUNT3_out
 );
 
 
 reg [3:0] COUNT1, COUNT2, COUNT3;
-//eg [3:0] SEG1_r, SEG2_r, SEG3_r, SEG4_r, SEG5_r, SEG6_r;
+
 initial begin
       COUNT1 <= 4'b0;//4bitの理由は0から9までのカウントのため
       COUNT2 <= 4'b0;
@@ -21,65 +22,6 @@ initial begin
       SEG6 <= 0;
 end
 
-/*always@(posedge CLK)begin
-  SEG1 <= SEG1_r;
-  SEG2 <= SEG2_r;
-  SEG3 <= SEG3_r;
-  SEG4 <= SEG4_r;
-  SEG5 <= SEG5_r;
-  SEG6 <= SEG6_r;
-end*/
-
-
-
-
-/*always@(posedge CLK)begin
-    if(RST)begin
-        SEG1 <= 0;
-        SEG2 <= 0;
-        SEG3 <= 0;
-        SEG4 <= 0;
-        SEG5 <= 0;
-        SEG6 <= 0;
-    end
-    else if(SEL_IN[0])begin
-        SEG1 <= COUNT1;
-        SEG2 <= COUNT1;
-    end
-    else if(SEL_IN[1])begin
-        SEG3 <= COUNT2;
-        SEG4 <= COUNT2;
-    end
-    else if(SEL_IN[2])begin
-        SEG5 <= COUNT3;
-        SEG6 <= COUNT3;
-    end
-    else if(RESULT_IN == 2'b01)begin//wrong
-      SEG1 <= 4'd1;
-      SEG2 <= 4'd1;
-      SEG3 <= 4'd1;
-      SEG4 <= 4'd1;
-      SEG5 <= 4'd1;
-      SEG6 <= 4'd1;
-	 end
-	 else if(RESULT_IN == 2'b11)begin//correct
-      SEG1 <= 4'd2;
-      SEG2 <= 4'd2;
-      SEG3 <= 4'd2;
-      SEG4 <= 4'd2;
-      SEG5 <= 4'd2;
-      SEG6 <= 4'd2;
-	 end
-	 else begin
-      SEG1 <= SEG1;
-      SEG2 <= SEG2;
-      SEG3 <= SEG3;
-      SEG4 <= SEG4;
-      SEG5 <= SEG5;
-      SEG6 <= SEG6;
-	end
-end*/
-
 always@(posedge CLK)begin
     if(RST)begin
         SEG1 <= 0;
@@ -89,7 +31,7 @@ always@(posedge CLK)begin
         SEG5 <= 0;
         SEG6 <= 0;
     end
-    else if(RESULT_IN == 2'b01)begin//wrong
+    else if(RESULT == 2'b01)begin//wrong
       SEG1 <= 4'd1;
       SEG2 <= 4'd1;
       SEG3 <= 4'd1;
@@ -97,7 +39,7 @@ always@(posedge CLK)begin
       SEG5 <= 4'd1;
       SEG6 <= 4'd1;
 	 end
-	 else if(RESULT_IN == 2'b11)begin//correct
+	 else if(RESULT == 2'b11)begin//correct
       SEG1 <= 4'd2;
       SEG2 <= 4'd2;
       SEG3 <= 4'd2;
@@ -108,27 +50,29 @@ always@(posedge CLK)begin
 	 else begin
       SEG1 <= COUNT1;
       SEG2 <= COUNT1;
+
       SEG3 <= COUNT2;
       SEG4 <= COUNT2;
+      
       SEG5 <= COUNT3;
       SEG6 <= COUNT3;
 	end
 end
 
 always @(posedge CLK) begin
-  if(SEL_IN[0])begin
+  if(SEL[0])begin
     COUNT1 <= COUNT1 + 1;
       if(COUNT1 == 9)begin
         COUNT1 <= 1;
       end
 	end	 
-	else if(SEL_IN[1])begin
+	else if(SEL[1])begin
 		COUNT2 <= COUNT2 + 1;
 		  if(COUNT2 == 9)begin
 		    COUNT2 <= 1;
 		  end
 	end	  
-	else if(SEL_IN[2])begin
+	else if(SEL[2])begin
 		COUNT3 <= COUNT3 + 1;
 		  if(COUNT3 == 9)begin
 		    COUNT3 <= 1;
@@ -137,7 +81,7 @@ always @(posedge CLK) begin
 end
 
 always @(posedge CLK)begin
-	if(DEC_IN)begin
+	if(DEC)begin
 		COUNT1_out <= COUNT1;
 		COUNT2_out <= COUNT2;
 		COUNT3_out <= COUNT3;
@@ -149,67 +93,5 @@ always @(posedge CLK)begin
    end
 
 end
-
-/*always @(posedge CLK)begin
-	if(RESULT_IN == 2'b01)begin
-      SEG1_r <= 4'd1;
-      SEG2_r <= 4'd1;
-      SEG3_r <= 4'd1;
-      SEG4_r <= 4'd1;
-      SEG5_r <= 4'd1;
-      SEG6_r <= 4'd1;
-	end
-	else if(RESULT_IN == 2'b11)begin
-      SEG1_r <= 4'd2;
-      SEG2_r <= 4'd2;
-      SEG3_r <= 4'd2;
-      SEG4_r <= 4'd2;
-      SEG5_r <= 4'd2;
-      SEG6_r <= 4'd2;
-	end
-	else begin
-      SEG1_r <= SEG1_r;
-      SEG2_r <= SEG2_r;
-      SEG3_r <= SEG3_r;
-      SEG4_r <= SEG4_r;
-      SEG5_r <= SEG5_r;
-      SEG6_r <= SEG6_r;
-	end
-end*/
-	
-//assign ANSER_SHEET = COUNT3 + COUNT2 + COUNT1;
-
-/*always @(posedge CLK) begin
-    //if(SEL_IN[0] == 1 || SEL_IN[1] == 1 || SEL_IN[2] == 1)begin//どこかがセレクトされた
-		if(INC_IN)begin
-			COUNT <= COUNT + 1;
-				if(COUNT == 10)begin
-					COUNT <= 1;
-				end    
-		//end
-	 end
-end*/
-
-/*always @(posedge CLK) begin
-	if(RST | CLR_IN)begin
-        SEG1 <= 0;
-        SEG2 <= 0;
-        SEG3 <= 0;
-        SEG4 <= 0;
-        SEG5 <= 0;
-        SEG6 <= 0;
-    end
-	 else begin
-		  SEG1 <= COUNT;
-        SEG2 <= COUNT;
-		  
-        SEG3 <= COUNT;
-        SEG4 <= COUNT;
-		  
-        SEG5 <= COUNT;
-        SEG6 <= COUNT;
-	end
-
-end*/
 
 endmodule
--- 1PTEST/READY.v
+++ 1PTEST/READY.v
@@ -1,7 +1,20 @@
 module READY(
   input CLK, RST, READY_1P,
   output reg[3:0] NUM
+  output reg RUN_IN;
 );//1HZイネーブル信号生成
+
+reg READY_2P;
+initial begin
+  READY_2P <= 1;//2P
+end
+always @(posedge CLK)begin
+  if(READY_1P == 1 && READY_2P == 1)                   //チャタリングを噛ますか考えて
+    RUN_IN <= 1;
+  else
+   RUN_IN <= 0;
+end
+
 reg [25:0] cnt;
 wire en1hz = (cnt == 26'd49_999_99);
 always @(posedge CLK)begin
@@ -25,7 +38,7 @@ always @(posedge CLK)begin
 end
 
 always @(posedge CLK)begin
-  if(READY_1P)
+  if(RUN_IN)
     NUM <= sec;
   else
     NUM <= NUM;
